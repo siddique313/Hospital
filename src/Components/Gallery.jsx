ae5ef6014841487e0636515b65a830f9b4f03be5
siddique 313 <siddiqueali531@gmail.com>
2025-02-06 17:44:30 +0000

    scrooll done

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -7,50 +7,17 @@ import img8 from "../assets/machine9.jpg";
 function Gallery() {
   return (
     <div className="container mx-auto">
-      <div
-        style={{
-          textAlign: "center",
-          padding: "20px",
-          marginTop: "20px",
-          color: "black",
-          fontSize: "1.5rem",
-        }}
-      >
-        <h1>Gallery</h1>
+      <div className="w-full text-center p-4 mt-5 text-black text-3xl">
+        <h1 className="font-serif">Gallery</h1>
       </div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gap: "10px",
-          padding: "10px",
-        }}
-      >
-        <img
-          style={{ width: "100%", objectFit: "cover", height: "240px" }}
-          src={img1}
-        />
+      <div className="grid grid-cols-3 gap-4 p-4">
+        <img className="w-full object-cover h-60" src={img1} />
 
-        <img
-          style={{ width: "100%", objectFit: "cover", height: "240px" }}
-          src={img4}
-        />
-        <img
-          style={{ width: "100%", objectFit: "cover", height: "240px" }}
-          src={img5}
-        />
-        <img
-          style={{ width: "100%", objectFit: "cover", height: "240px" }}
-          src={img6}
-        />
-        <img
-          style={{ width: "100%", objectFit: "cover", height: "240px" }}
-          src={img7}
-        />
-        <img
-          style={{ width: "100%", objectFit: "cover", height: "240px" }}
-          src={img8}
-        />
+        <img className="w-full object-cover h-60" src={img4} />
+        <img className="w-full object-cover h-60" src={img5} />
+        <img className="w-full object-cover h-60" src={img6} />
+        <img className="w-full object-cover h-60" src={img7} />
+        <img className="w-full object-cover h-60" src={img8} />
       </div>
     </div>
   );
