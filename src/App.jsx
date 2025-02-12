42df5f31acccf976be63cb6b789405b955b8fc6c
siddique 313 <siddiqueali531@gmail.com>
2025-02-12 16:44:20 +0000

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { HomeScreen } from "./Components/HomeScreen";
+import HomeScreenImage from "./Components/HomeScreenImage";
+import Categories from "./Components/Categories";
 
 const App = () => {
   return (
     <div>
       <HomeScreen />
+      <HomeScreenImage />
+      <Categories />
     </div>
   );
 };
---
+++ src/Components/HomeScreenImage.jsx
@@ -0,0 +1,6 @@
+import img from "../assets/image.jpeg";
+function HomeScreenImage() {
+  return <img className="w-full p-5" src={img} alt="" />;
+}
+
+export default HomeScreenImage;
