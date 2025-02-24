35046d3e5ad85029cfba38f57c3ea02f80740b37
siddique 313 <siddiqueali531@gmail.com>
2025-02-24 19:01:20 +0000

    contact updat

--- src/Contact/sechema/index.jsx
+++ src/Contact/sechema/index.jsx
@@ -3,9 +3,5 @@ import * as Yup from "yup";
 export const SignUpSchema = Yup.object({
   name: Yup.string().min(4).max(20).required("Pleas filled a required name"),
   email: Yup.string().email().required("Pleas filled a required email"),
-  password: Yup.string().required("Pleas filled a required password").min(8),
-  confirm_password: Yup.string()
-    .required("Pleas filled a required confirm_password")
-    .min(8)
-    .oneOf([Yup.ref("password"), null, "password must match"]),
+  textArea: Yup.string(),
 });
